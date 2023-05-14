18ad7cb54e04991897ce5e01a12246c783d75ed0
Joseph Cross <joseph.cross@atmosphere.us>
2023-05-14 13:08:30 -0400

    adjust routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,32 @@ const router = createRouter({
 			redirect: { name: 'arb' }
 		},
 		{
+			path: '/oa',
+			redirect: { name: 'oa-arb' },
+			children: [
+				{
+					path: '/arb',
+					name: 'oa-arb',
+					component: () => import('../views/Arb.vue')
+				},
+				{
+					path: '/free-bet',
+					name: 'oa-free-bet',
+					component: () => import('../views/FreeBet.vue')
+				},
+				{
+					path: '/risk-free',
+					name: 'oa-risk-free',
+					component: () => import('../views/RiskFree.vue')
+				},
+				{
+					path: '/devig',
+					name: 'oa-devig',
+					component: () => import('../views/Devig.vue')
+				},
+			]
+		},
+		{
 			path: '/arb',
 			name: 'arb',
 			component: () => import('../views/Arb.vue')
