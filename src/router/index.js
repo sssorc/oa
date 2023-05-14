e4810a010498fdcaf4fee0632b1e718968e781e3
Joseph Cross <joseph.cross@atmosphere.us>
2023-05-14 12:24:30 -0400

    change base to /oa/

--- src/router/index.js
+++ src/router/index.js
@@ -6,28 +6,32 @@ const router = createRouter({
 		{
 			path: '/',
 			name: 'home',
-			component: () => import('../views/Arb.vue'),
 			redirect: { name: 'arb' }
 		},
 		{
-			path: '/arb',
-			name: 'arb',
-			component: () => import('../views/Arb.vue')
-		},
-		{
-			path: '/free-bet',
-			name: 'free-bet',
-			component: () => import('../views/FreeBet.vue')
-		},
-		{
-			path: '/risk-free',
-			name: 'risk-free',
-			component: () => import('../views/RiskFree.vue')
-		},
-		{
-			path: '/devig',
-			name: 'devig',
-			component: () => import('../views/Devig.vue')
+			path: '/oa',
+			children: [
+				{
+					path: '/arb',
+					name: 'arb',
+					component: () => import('../views/Arb.vue')
+				},
+				{
+					path: '/free-bet',
+					name: 'free-bet',
+					component: () => import('../views/FreeBet.vue')
+				},
+				{
+					path: '/risk-free',
+					name: 'risk-free',
+					component: () => import('../views/RiskFree.vue')
+				},
+				{
+					path: '/devig',
+					name: 'devig',
+					component: () => import('../views/Devig.vue')
+				},
+			]
 		},
 	]
 })
