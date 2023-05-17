bf34cc73424c9f6b5b8f7d517facf906496d5576
Joseph Cross <joseph.cross@atmosphere.us>
2023-05-17 13:12:29 -0400

    add vue-gtag

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(VueGtag, {
+	config: { id: "G-LPV4BWE4E7" }
+})
 
 app.mount('#app')
