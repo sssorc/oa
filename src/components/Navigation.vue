acb26ed23457c25d8c5d5b3efe11276eeef17b9e
Joseph Cross <joseph.cross@atmosphere.us>
2023-06-06 18:42:51 -0400

    add devig nav item

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,6 +5,7 @@
 			<RouterLink :class="{'router-link-active': $route.fullPath == '/arb' }" to="/arb" class="flex-center">Arbitrage</RouterLink>
 			<RouterLink :class="{'router-link-active': $route.fullPath == '/free-bet' }" to="/free-bet" class="flex-center">Free bet</RouterLink>
 			<RouterLink :class="{'router-link-active': $route.fullPath == '/risk-free' }" to="/risk-free" class="flex-center">Risk-free</RouterLink>
+			<RouterLink :class="{'router-link-active': $route.fullPath == '/devig' }" to="/devig" class="flex-center">Devig</RouterLink>
 		</nav>
 	</header>
 </template>
