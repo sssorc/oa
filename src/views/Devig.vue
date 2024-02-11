8bb7e4453521ef059c0059379b4433dca2175522
Joseph Cross <joseph.cross@atmosphere.us>
2024-02-10 22:20:22 -0500

    add masthead to devig page

--- src/views/Devig.vue
+++ src/views/Devig.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import '@/assets/css/main.scss'
+import Navigation from '@/components/Navigation.vue';
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
 
@@ -40,6 +41,9 @@ document.addEventListener('click', function(e) {
 }, { once: true } );
 
 export default {
+	components: {
+		Navigation
+	},
 	data() {
 		return {
 			apiUrl: '//api.crazyninjaodds.com/api/devigger/v1/sportsbook_devigger.aspx?api=open&Args=ev_p,fb_p,fo_o,kelly,dm&',
@@ -297,19 +301,13 @@ export default {
 
 <template>
 	<div class="devig">
-		<header class="section full-width py-12 bg-blue show-md">
-			<nav class="flex-center wrap gap-x-32 gap-y-16 contain">
-				<a id="HyperLinkArbCalculator" target="_blank" href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_calculator.aspx">Hedge Calculator</a>
-				<a id="HyperLinkDevigger" target="_blank" href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_devigger.aspx">Devigger</a>
-				<a id="HyperLinkComboBreaker" target="_blank" href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_combo_breaker.aspx">C-C-C-Combo Breaker</a>
-				<a id="HyperLinkUpByXPromo" target="_blank" href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_UpByXPromo.aspx">Up By X Promo Simulator</a>
-				<a id="HyperLinkCNMSignup" target="_blank" href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_CNM_signup.aspx">CrazyNinjaOdds.com</a>
-			</nav>
-		</header>
+		<div class="legacy">
+			<Navigation/>
+		</div>
 		<section :class="{'show-settings': showSettings}" class="rel layout">
 			<main>
 				<div class="flex-split gap-16 mb-32">
-					<h1>CNM Devigger</h1>
+					<h1><a href="http://www.crazyninjamike.com/Public/sportsbooks/sportsbook_calculator.aspx" style="text-decoration:none">CNM Devigger</a></h1>
 					<button @click="showSettings = true" class="flex-center toggle-settings reset hide-md">
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 512 512" fill="currentColor"><path d="M496 384H160v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h80v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h336c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160h-80v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h336v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h80c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160H288V48c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16C7.2 64 0 71.2 0 80v32c0 8.8 7.2 16 16 16h208v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h208c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16z"/></svg>
 					</button>
