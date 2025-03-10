34bc9ac6695fc658f024fd6ded67b6dac94abbb9
Joseph Cross <joseph.cross@atmosphere.us>
2025-03-09 22:18:48 -0400

    change redirect

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            redirect: { name: 'arbitrage' },
+            redirect: { name: 'devig' },
         },
         {
             path: '/oa',
