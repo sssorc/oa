5558ac949788a52de219134c0bf4d02f3222123f
Joseph Cross <joseph.cross@atmosphere.us>
2025-05-15 19:54:21 -0400

    update GA ID; add meta description

--- src/main.js
+++ src/main.js
@@ -14,7 +14,15 @@ app.use(router);
 app.use(store);
 app.use(head);
 app.use(VueGtag, {
-    config: { id: 'G-LPV4BWE4E7' },
+    config: {
+        id: 'G-LPV4BWE4E7', // Replace with your new GA4 measurement ID
+        params: {
+            send_page_view: true,
+            anonymize_ip: true,
+        },
+    },
+    appName: 'HedgeCalc',
+    pageTrackerScreenviewEnabled: true,
 });
 
 app.mount('#app');
