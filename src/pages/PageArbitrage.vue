12563292a035d764c01e2e4b36f2ba259f6f4ed1
Joseph Cross <joseph.cross@atmosphere.us>
2025-05-15 19:21:17 -0400

    adjust arb copy

--- src/pages/PageArbitrage.vue
+++ src/pages/PageArbitrage.vue
@@ -183,12 +183,15 @@ onMounted(() => {
         </section>
         <section class="mx-auto mt-10 max-w-7xl px-5">
             <h5 class="border-jet font-space text-jet mb-3 border-b pb-2 font-mono font-bold">How to use</h5>
-            <p>Use the arbitrage calculator to find the optimal amount to place on a hedge or arbitrage bet.</p>
-            <ul class="marker:text-slate mt-3 list-disc space-y-2 pl-4 text-sm marker:text-xs">
-                <li>Enter your original bet amount and odds as <em>Stake A</em> and <em>Odds A</em></li>
-                <li>Enter the odds of your hedge bet as <em>Odds B</em></li>
-                <li>Enter additional odds as <em>Odds C</em> for a three-way hedge</li>
-            </ul>
+            <div class="space-y-3">
+                <p>Use the arbitrage calculator to find the optimal amount to place on a hedge or arbitrage bet.</p>
+                <ul class="marker:text-slate list-disc space-y-2 pl-4 text-sm marker:text-xs">
+                    <li>Enter your first bet amount and odds as <em>Stake A</em> and <em>Odds A</em></li>
+                    <li>Enter the odds of your second or hedge bet as <em>Odds B</em></li>
+                    <li>Enter additional odds as <em>Odds C</em> for a three-way arb</li>
+                </ul>
+                <p>Plus signs are optional in odds fields.</p>
+            </div>
         </section>
     </div>
 </template>
