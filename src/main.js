2c7fed72c31dd036d4242991ae6087f095391716
Joseph Cross <joseph.cross@atmosphere.us>
2025-05-18 14:30:01 -0400

    remove gtag code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createHead } from '@vueuse/head';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VueGtag from 'vue-gtag';
 import '@/assets/css/main.scss';
 import './assets/css/style.css';
 
@@ -13,8 +12,5 @@ const head = createHead();
 app.use(router);
 app.use(store);
 app.use(head);
-app.use(VueGtag, {
-    config: { id: 'G-LPV4BWE4E7' },
-});
 
 app.mount('#app');
