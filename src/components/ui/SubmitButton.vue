822cebc9f123718bcc5859b43bc6a81eb33aefcc
Joseph Cross <joseph.cross@atmosphere.us>
2025-09-06 15:54:10 -0400

    adjust submit button active state

--- src/components/ui/SubmitButton.vue
+++ src/components/ui/SubmitButton.vue
@@ -13,7 +13,7 @@ defineProps({
     <button
         type="submit"
         :class="{ 'from-pale-blue to-ice-blue pointer-events-none from-10%': isSubmitting }"
-        class="from-pale-blue to-ice-blue text-jet relative cursor-pointer bg-linear-65 from-40% px-8 py-3 font-mono tracking-wide shadow transition-all duration-100 hover:from-10% active:translate-x-[1px] active:translate-y-[1px] active:shadow-none disabled:translate-none disabled:cursor-not-allowed disabled:from-[#CFDDE8] disabled:to-[#C4E3E0] disabled:text-gray-500 disabled:shadow dark:disabled:from-gray-400 dark:disabled:to-gray-400 dark:disabled:text-gray-700"
+        class="from-pale-blue to-ice-blue text-jet relative scale-100 cursor-pointer bg-linear-65 from-40% px-8 py-3 font-mono tracking-wide shadow transition-all duration-100 hover:from-10% active:translate-y-[1px] active:scale-99 active:shadow-none disabled:translate-none disabled:cursor-not-allowed disabled:from-[#CFDDE8] disabled:to-[#C4E3E0] disabled:text-gray-500 disabled:shadow dark:disabled:from-gray-400 dark:disabled:to-gray-400 dark:disabled:text-gray-700"
         :style="{ minWidth: '170px' }"
     >
         <LoadingSpinner v-if="isSubmitting" size="24" class="text-jet absolute inset-0 flex items-center justify-center" />
